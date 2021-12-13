39879a452100e847aea39b7cc4e6bafc52e11d8b
BrianKThomas <kanaaibrian25@gmail.com>
2021-12-13 13:25:14 +0300

    #$%$%%$$%

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 window.onload =()=>{
     // dynamically add target _blank to all anchor tags because you forgot
-    const allhrefs = document.querySelectorAll('a');
+    const allhrefs = document.querySelectorAll('a:not(.navbar-link)');
     allhrefs.forEach(href => {
          href.setAttribute("target","_blank");
     });
 }
+
+const alllinkedButtons = document.querySelectorAll('a button');
+alllinkedButtons.forEach(button => {
+    const parent = button.parentElement;
+    parent.classList.add('blank');
+});
